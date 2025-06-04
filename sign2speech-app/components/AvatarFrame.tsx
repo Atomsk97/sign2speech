a019a20e09d990327c8bbf4cd919e98033877233
Sebastian <sperezr@uni.pe>
2025-06-04 12:49:43 -0500

    ref: AvatarFrame use dynamic image sources and dimensions

--- sign2speech-app/components/AvatarFrame.tsx
+++ sign2speech-app/components/AvatarFrame.tsx
@@ -1,15 +1,31 @@
 import { Image, View } from "react-native";
 
-export default function AvatarFrame() {
+const images: { [key: string]: any } = {
+  "1": require("@/assets/images/3DAvatar_placeholder_1.png"),
+  "2": require("@/assets/images/3DAvatar_placeholder_2.png"),
+  "3": require("@/assets/images/3DAvatar_placeholder_3.png"),
+};
+
+export default function AvatarFrame(props: {
+  width?: number;
+  height?: number;
+  option?: string;
+}) {
+  //{ width: 250, height: 518 }
   // This component is a placeholder for the 3D avatar frame.
+  const { width = 250, height = 518 } = props;
+  const imageKey = props.option || "1";
+  const imageSource = images[imageKey];
   return (
     <View>
       <Image
-        source={require("@/assets/images/3DAvatar_placeholder_1.png")}
-        style={{ width: 250, height: 518, alignSelf: "center" }}
+        source={imageSource}
+        style={{
+          width: width,
+          height: height,
+          alignSelf: "center",
+        }}
       />
     </View>
   );
 }
-
-//76x133
