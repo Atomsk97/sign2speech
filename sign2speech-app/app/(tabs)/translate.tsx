3de0b1c33c649772480022dd1ca2739f33fe9d0f
Sebastian <sperezr@uni.pe>
2025-06-04 01:25:03 -0500

    feat: enhance TranslateScreen with keyboard-aware scrolling

--- sign2speech-app/app/(tabs)/translate.tsx
+++ sign2speech-app/app/(tabs)/translate.tsx
@@ -1,28 +1,77 @@
 import AvatarFrame from "@/components/AvatarFrame";
-import { Feather } from "@expo/vector-icons";
-import { TextInput, View } from "react-native";
+import { Feather, SimpleLineIcons } from "@expo/vector-icons";
+import { useState } from "react";
+import {
+  TextInput,
+  Text,
+  View,
+  TouchableHighlight,
+  Keyboard,
+} from "react-native";
+
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 export default function TranslateScreen() {
+  const [translatedText, setTranslatedText] = useState<string>("");
+
   return (
-    <View className="bg-secondary h-full px-4 py-16 items-center justify-center">
-      <View className="bg-primary/50 w-full mx-4 my-2 items-center justify-center rounded-2xl">
+    <KeyboardAwareScrollView
+      className="flex-1 bg-secondary"
+      contentContainerStyle={{
+        flexGrow: 1,
+        paddingHorizontal: 16,
+        paddingTop: 32,
+        paddingBottom: 8,
+      }}
+      bounces={false}
+      extraScrollHeight={142}
+      enableOnAndroid={true}
+      enableAutomaticScroll={true}
+      onScrollBeginDrag={() => Keyboard.dismiss()}
+      keyboardShouldPersistTaps="handled"
+    >
+      <View className="bg-primary/50 w-full mb-4 self-center items-center justify-center rounded-2xl overflow-hidden">
+        <View className="w-full flex-row items-center justify-between mt-2">
+          <Text
+            className="text-primary font-nerko text-2xl"
+            style={{
+              left: "50%",
+              transform: [{ translateX: "-50%" }],
+            }}
+          >
+            {translatedText}
+          </Text>
+          <TouchableHighlight
+            className="bg-primary/80 rounded-full p-4 me-2"
+            onPress={() => {
+              console.log("Open notebook pressed");
+            }}
+          >
+            <SimpleLineIcons name="notebook" size={24} color={"white"} />
+          </TouchableHighlight>
+        </View>
         <AvatarFrame />
       </View>
-      <View className="flex-row items-center justify-between mt-4 gap-x-2">
+
+      <View className="flex-row items-center justify-between gap-x-2">
         <View className="flex-row items-center justify-between bg-primary/50 rounded-full px-4 py-2 w-5/6 gap-x-2">
           <TextInput
-            className="text-xl"
+            className="text-xl flex-1"
             placeholderTextColor={"#DBE9EE"}
             placeholder="Escribe para traducir..."
-          ></TextInput>
-          <View className="bg-primary/80 rounded-full p-4">
+            enterKeyHint="send"
+            onSubmitEditing={(e) => {
+              setTranslatedText(e.nativeEvent.text.trim());
+            }}
+          />
+          <TouchableHighlight className="bg-primary/80 rounded-full p-4">
             <Feather name="camera" size={24} color={"white"} />
-          </View>
+          </TouchableHighlight>
         </View>
-        <View className="self-center bg-primary/80 rounded-full p-4">
+        <TouchableHighlight className="self-center bg-primary/80 rounded-full p-4">
           <Feather name="mic" size={24} color={"white"} />
-        </View>
+        </TouchableHighlight>
       </View>
-    </View>
+    </KeyboardAwareScrollView>
   );
 }
--- sign2speech-app/components/AvatarFrame.tsx
+++ sign2speech-app/components/AvatarFrame.tsx
@@ -1,26 +1,12 @@
-import { Image, Text, View } from "react-native";
-import { SimpleLineIcons } from "@expo/vector-icons";
+import { Image, View } from "react-native";
 
 export default function AvatarFrame() {
+  // This component is a placeholder for the 3D avatar frame.
   return (
-    <View className="h-full w-full p-2 flex-col overflow-hidden">
-      <View className="flex-row items-center justify-between">
-        <Text
-          className="text-primary font-nerko text-2xl"
-          style={{
-            left: "50%",
-            transform: [{ translateX: "-50%" }],
-          }}
-        >
-          Hola
-        </Text>
-        <View className="bg-primary/80 rounded-full p-4 me-2">
-          <SimpleLineIcons name="notebook" size={24} color={"white"} />
-        </View>
-      </View>
+    <View>
       <Image
         source={require("@/assets/images/3DAvatar_placeholder_1.png")}
-        style={{ width: 264, height: 534, alignSelf: "center" }}
+        style={{ width: 250, height: 518, alignSelf: "center" }}
       />
     </View>
   );
